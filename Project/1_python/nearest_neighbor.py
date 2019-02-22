1071919bb22251fcfa15c6f06db9e4e15db6683e
Ali Nazari <ali.nazari@aut.ac.ir>
2019-02-22 11:26:13 +0330

    fill the array with vertices in the file

--- Project/1_python/nearest_neighbor.py
+++ Project/1_python/nearest_neighbor.py
@@ -3,4 +3,12 @@ import numpy as np
 f = open("points.txt", "r")
 
 n = int(f.readline())
-vertices = np.zeros((n, 2))
+vertices = np.zeros((n, 3))
+
+for line, i in zip(f, range(n)):
+    a_line = str(line).split(' ')
+    vertices[i][0] = int(a_line[0])
+    vertices[i][1] = int(a_line[1])
+
+print(vertices)
+
