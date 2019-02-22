ff5341ef43562d3eaea17b0d240039d1ef30dd11
Ali Nazari <ali.nazari@aut.ac.ir>
2019-02-22 13:11:30 +0330

    nearest neighbor implementation completed

--- Project/1_python/nearest_neighbor.py
+++ Project/1_python/nearest_neighbor.py
@@ -1,4 +1,19 @@
 import numpy as np
+from math import hypot as hp
+
+
+def find_next_nearest_neighbor(present_location):
+    min_distance = float("inf")
+    selected_point = 0
+    for k in range(1, n):
+        if vertices[k][2] == 1:
+            continue
+        d = hp(vertices[present_location][0] - vertices[k][0], vertices[present_location][1] - vertices[k][1])
+        if d < min_distance:
+            min_distance = d
+            selected_point = k
+    return selected_point
+
 
 f = open("points.txt", "r")
 
@@ -10,5 +25,10 @@ for line, i in zip(f, range(n)):
     vertices[i][0] = int(a_line[0])
     vertices[i][1] = int(a_line[1])
 
-print(vertices)
-
+vertices[0][2] = 1
+present_point = 0
+print(present_point)
+while np.sum(vertices, axis=0)[2] != n:
+    present_point = find_next_nearest_neighbor(present_point)
+    vertices[present_point][2] = 1
+    print(present_point)
