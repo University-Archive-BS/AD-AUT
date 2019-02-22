70fab5742a67c9e8d872cf07cb8fc7ac3fac9848
Ali Nazari <ali.nazari@aut.ac.ir>
2019-02-22 14:55:43 +0330

    implement exhaustive algorithm completely

--- Project/1_python/nearest_neighbor.py
+++ Project/1_python/nearest_neighbor.py
@@ -2,33 +2,41 @@ import numpy as np
 from math import hypot as hp
 
 
+def nearest_neighbor():
+    points[0][2] = 1
+    present_point = 0
+    result_path = [present_point + 1]
+    while np.sum(points, axis=0)[2] != n:
+        present_point = find_next_nearest_neighbor(present_point)
+        points[present_point][2] = 1
+        result_path.append(present_point + 1)
+    return result_path
+
+
 def find_next_nearest_neighbor(present_location):
     min_distance = float("inf")
     selected_point = 0
     for k in range(1, n):
-        if vertices[k][2] == 1:
+        if points[k][2] == 1:
             continue
-        d = hp(vertices[present_location][0] - vertices[k][0], vertices[present_location][1] - vertices[k][1])
+        d = hp(points[present_location][0] - points[k][0], points[present_location][1] - points[k][1])
         if d < min_distance:
             min_distance = d
             selected_point = k
     return selected_point
 
 
+def fill_array_by_file(file):
+    vertices = np.zeros((n, 3))
+    for line, i in zip(file, range(n)):
+        a_line = str(line).split(' ')
+        vertices[i][0] = int(a_line[0])
+        vertices[i][1] = int(a_line[1])
+    return vertices
+
+
 f = open("points.txt", "r")
-
 n = int(f.readline())
-vertices = np.zeros((n, 3))
-
-for line, i in zip(f, range(n)):
-    a_line = str(line).split(' ')
-    vertices[i][0] = int(a_line[0])
-    vertices[i][1] = int(a_line[1])
-
-vertices[0][2] = 1
-present_point = 0
-print(present_point)
-while np.sum(vertices, axis=0)[2] != n:
-    present_point = find_next_nearest_neighbor(present_point)
-    vertices[present_point][2] = 1
-    print(present_point)
+points = fill_array_by_file(f)
+result = nearest_neighbor()
+print(result)
