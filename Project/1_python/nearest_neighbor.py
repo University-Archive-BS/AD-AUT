2e0808c2156ae828d1c41cc7c656a3b8d41bf79d
Ali Nazari <ali.nazari@aut.ac.ir>
2019-02-23 15:26:48 +0330

    improve output of the nearest neighbor

--- Project/1_python/nearest_neighbor.py
+++ Project/1_python/nearest_neighbor.py
@@ -5,17 +5,23 @@ from math import hypot as hp
 def nearest_neighbor():
     points[0][2] = 1
     present_point = 0
+    cost_path = 0.0
     result_path = [present_point + 1]
+    print('1. ' + str(points[present_point][0]) + ', ' + str(points[present_point][1]))
     for i in range(n - 1):
-        present_point = find_next_nearest_neighbor(present_point)
+        present_point, temp_cost = find_next_nearest_neighbor(present_point)
+        cost_path += temp_cost
         points[present_point][2] = 1
         result_path.append(present_point + 1)
-    return result_path
+        print(str(i + 2) + '. ' + str(points[present_point][0]) + ', ' + str(points[present_point][1]))
+    cost_path += hp(points[present_point][0] - points[0][0], points[present_point][1] - points[0][1])
+    return cost_path
 
 
 def find_next_nearest_neighbor(present_location):
     min_distance = float("inf")
     selected_point = 0
+    this_cost = 0
     for k in range(1, n):
         if points[k][2] == 1:
             continue
@@ -23,7 +29,8 @@ def find_next_nearest_neighbor(present_location):
         if d < min_distance:
             min_distance = d
             selected_point = k
-    return selected_point
+            this_cost = d
+    return selected_point, this_cost
 
 
 def fill_array_by_file(file):
@@ -38,5 +45,5 @@ def fill_array_by_file(file):
 f = open("points.txt", "r")
 n = int(f.readline())
 points = fill_array_by_file(f)
-result = nearest_neighbor()
-print(result)
+print(nearest_neighbor())
+
