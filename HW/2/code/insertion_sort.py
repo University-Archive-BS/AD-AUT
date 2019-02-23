7552caea783e6143c6c36257b4a317198ca75a2c
Ali Nazari <ali.nazari@aut.ac.ir>
2019-02-23 21:54:29 +0330

    fix bug in selection sort

--- HW/2/code/insertion_sort.py
+++
@@ -1,9 +0,0 @@
-a = []
-n = 2
-for i in range(1, n):
-    key = a[i]
-    j = i - 1
-    while j >= 0 and a[j] > key:
-        a[j + 1] = a[j]
-        j -= 1
-    a[j + 1] = key
---
+++ HW/2/code/sort.py
@@ -0,0 +1,24 @@
+def insertion_sort(an_array):
+    for i in range(1, an_array.__len__()):
+        key = an_array[i]
+        j = i - 1
+        while j >= 0 and an_array[j] > key:
+            an_array[j + 1] = an_array[j]
+            j -= 1
+        an_array[j + 1] = key
+    return an_array
+
+
+def selection_sort(an_array):
+    for i in range(an_array.__len__()):
+        min_index = i
+        for j in range(i + 1, an_array.__len__()):
+            if an_array[j] < an_array[i]:
+                min_index = j
+        an_array[i], an_array[min_index] = an_array[min_index], an_array[i]
+    return an_array
+
+
+print(selection_sort([2, 3, 5, 6]))
+
+
